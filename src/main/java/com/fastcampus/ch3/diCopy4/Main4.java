8ecbf6d742daa6454b656df48663e7b5506d1a4c
Robert0623 <tone8943@gmail.com>
2022-06-10 18:55:05 +0900

    ch3-3 Spring DI - 3 @Component, @Autowired, @Resource

--- src/main/java/com/fastcampus/ch3/diCopy4/Main4.java
+++ src/main/java/com/fastcampus/ch3/diCopy4/Main4.java
@@ -1,20 +1,36 @@
-package com.fastcampus.ch3.diCopy3;
+package com.fastcampus.ch3.diCopy4;
 
 import com.google.common.reflect.ClassPath;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
-import java.io.FileReader;
-import java.io.IOException;
+import javax.annotation.Resource;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 
-@Component class Car {}
+@Component class Car {
+    @Resource
+    Engine engine;
+//    @Resource
+    Door door;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "engine=" + engine +
+                ", door=" + door +
+                '}';
+    }
+}
 @Component class SportsCar extends Car {}
 @Component class Truck extends Car {}
-@Component class Engine {}
+@Component
+class Engine {}
+@Component
+class Door {}
 
 class AppContext {
     Map map; //객체 저장소
@@ -22,7 +38,40 @@ class AppContext {
     AppContext() {
         map = new HashMap();
         doComponentScan();
+        doAutowired();
+        doResource();
     }
+
+    private void doResource() {
+        //map에 저장된 객체의 iv중에 @Reousrce 붙어 있으면
+        //map에서 iv의 이름에 맞는 객체를 찾아서 연결(객체의 주소를 iv에 저장)
+        try {
+            for (Object bean : map.values()) {
+                for(Field fld : bean.getClass().getDeclaredFields()){
+                    if(fld.getAnnotation(Resource.class)!=null) //byName
+                        fld.set(bean, getBean(fld.getName())); //car.engine = obj;
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doAutowired() {
+        //map에 저장된 객체의 iv중에 @Autowired가 붙어 있으면
+        //map에서 iv의 타입에 맞는 객체를 찾아서 연결(객체의 주소를 iv에 저장)
+        try {
+            for (Object bean : map.values()) {
+                for(Field fld : bean.getClass().getDeclaredFields()){
+                    if(fld.getAnnotation(Autowired.class)!=null)
+                        fld.set(bean, getBean(fld.getType())); //car.engine = obj;
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void doComponentScan() {
         try {
             //1. 패키지내의 클래스 목록을 가져온다.
@@ -31,7 +80,7 @@ class AppContext {
             ClassLoader classLoader = AppContext.class.getClassLoader();
             ClassPath classPath = ClassPath.from(classLoader);
 
-            Set<ClassPath.ClassInfo> set = classPath.getTopLevelClasses("com.fastcampus.ch3.diCopy3");
+            Set<ClassPath.ClassInfo> set = classPath.getTopLevelClasses("com.fastcampus.ch3.diCopy4");
 
             for (ClassPath.ClassInfo classInfo : set) {
                 Class clazz = classInfo.load();
@@ -60,14 +109,19 @@ class AppContext {
 }
 
 
-    public class Main3 {
+    public class Main4 {
     public static void main(String[] args) throws Exception {
         AppContext ac = new AppContext();
         Car car = (Car) ac.getBean("car"); //byName으로 객체를 검색
         Car car2 = (Car) ac.getBean(Car.class); //byType으로 객체를 검색
         Engine engine = (Engine) ac.getBean("engine");
+        Door door = (Door) ac.getBean(Door.class);
+
+//        car.engine = engine;
+//        car.door = door;
         System.out.println("car = " + car);
         System.out.println("car2 = " + car2);
         System.out.println("engine = " + engine);
+        System.out.println("door = " + door);
     }
 }
